7ad671077ebf02d5930cf359be59adc1c2e2467d
Warmybloodwolf <wyh040217@sjtu.edu.cn>
2024-12-27 20:22:07 +0800

    feat(RT): 新增3D装箱问题解决方案框架及相关模块
    新增了用于解决3D装箱问题的动态多模态深度强化学习框架（DMRL-BPP），包括动态多模态编码器、选择解码器、旋转解码器、打包放置方法等模块。同时引入了新的奖励函数以解决稀疏奖励问题，并采用Soft Actor-Critic算法进行模型训练。

---
+++ RT/DataHandler.py
@@ -0,0 +1,97 @@
+import csv
+import numpy as np
+from Params import args
+import random
+
+def BPP_generator(box_num, bin_x, bin_y, bin_z):
+    I = [
+        {
+            "l": bin_x,   # 物品长
+            "w": bin_y,   # 物品宽
+            "h": bin_z,   # 物品高
+        }
+    ]
+    # 在达到N之前持续分裂物品
+    while len(I) < box_num:
+        # 按体积加权从I中选取物品
+        volumes = [item["l"] * item["w"] * item["h"] for item in I]
+        total_volume = sum(volumes)
+        r = random.random() * total_volume
+        cumulative = 0.0
+        chosen_index = None
+        for idx, vol in enumerate(volumes):
+            cumulative += vol
+            if r <= cumulative:
+                chosen_index = idx
+                break
+        
+        # 选中的物品
+        chosen_item = I.pop(chosen_index)
+        
+        # 根据边长选择分割轴(以边长为权重)
+        edges = [chosen_item["l"], chosen_item["w"], chosen_item["h"]]
+        edge_sum = sum(edges)
+        r_edge = random.random() * edge_sum
+        cumulative_edge = 0.0
+        axis = None  # 0->x, 1->y, 2->z
+        for i, e_len in enumerate(edges):
+            cumulative_edge += e_len
+            if r_edge <= cumulative_edge:
+                axis = i
+                break
+        
+        # 沿所选轴方向的中点附近随机选择分割位置
+        # 为了保证不会出现太极端的值，以物品一半长度为参考，在中点附近±1/4范围随机选取
+        axis_length = edges[axis]
+        half_axis_length = axis_length / 2.0
+        split_offset = int((random.random() - 0.5) * (axis_length / 2.0))
+        split_pos1 = half_axis_length + split_offset
+        split_pos2 = half_axis_length - split_offset
+        
+        # 将物品分割为两个子物品
+        if axis == 0:
+            child1 = {"l": split_pos1,"w": chosen_item["w"],"h": chosen_item["h"]}
+            child2 = {"l": split_pos2,"w": chosen_item["w"],"h": chosen_item["h"]}
+        if axis == 1:
+            child1 = {"l": chosen_item["l"],"w": split_pos1,"h": chosen_item["h"]}
+            child2 = {"l": chosen_item["l"],"w": split_pos2,"h": chosen_item["h"]}
+        if axis == 2:    
+            child1 = {"l": chosen_item["l"],"w": chosen_item["w"],"h": split_pos1}
+            child2 = {"l": chosen_item["l"],"w": chosen_item["w"],"h": split_pos2}
+        
+        # 旋转子物品
+        def rotate_item(l, w, h):
+            # 随机旋转操作：随机打乱(l,w,h)顺序
+            dims = [l, w, h]
+            random.shuffle(dims)
+            return dims[0], dims[1], dims[2]
+        
+        child1["l"], child1["w"], child1["h"] = rotate_item(child1["l"], child1["w"], child1["h"])
+        child2["l"], child2["w"], child2["h"] = rotate_item(child2["l"], child2["w"], child2["h"])
+
+        # 加入到I中
+        I.append(child1)
+        I.append(child2)
+    
+    array = np.array([[d["l"], d["w"], d["h"]] for d in I])
+    return array
+
+class DataHandler:
+    def __init__(self):
+        if args.dataset_path is None:
+            # generate dataset: 
+            self.datalist = []
+            if args.box_min >= args.box_max:
+                raise ValueError('min should be less than max.')
+            steps = int(np.ceil(args.dataset_size / args.batch))
+            for i in range(steps):
+                N = random.randint(args.box_min, args.box_max)
+                # batch_size, box_num, 3
+                batch_data = np.array([BPP_generator(N, args.bin_x, args.bin_y, args.bin_z) for _ in range(min(args.batch, args.dataset_size - i * args.batch))])
+                self.datalist.append(batch_data)
+        else:
+            # read dataset from csv
+            # TODO: implement reading dataset from csv
+            # TODO: init args such as box_num, dataset_size
+            raise NotImplementedError('Dataset not supported yet.')
+        
---
+++ RT/engine.py
@@ -0,0 +1,237 @@
+import torch
+import torch.optim as optim
+from model import *
+import numpy as np
+import utils
+from Params import args
+from DataHandler import DataHandler
+from state import StatePack3D
+from pack import pack_step
+from model import build_model
+from utils import move_to, clip_grad_norms, explained_variance
+from tqdm import tqdm
+import logging
+
+class trainer():
+    def __init__(self):
+        self.handler = DataHandler()
+        self.model = build_model()
+        critic_params = [param for name, param in self.model['critic'].named_parameters() if 'log_alpha' not in name]
+        self.optimizer = optim.Adam([
+                {'params': self.model['actor'].parameters()},
+                {'params': critic_params, 'lr': args.critic_lr},
+                {'params': self.model['critic'].log_alpha, 'lr': args.critic_lr}
+            ], lr=args.actor_lr)
+        lambda1 = lambda epoch: min(1, epoch / args.lr_warmup)
+        self.scheduler = optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda=lambda1)
+        self.loglikelihood = utils._calc_log_likelihood_3d
+        self.entropy = utils._calc_entropy_3d
+
+    def train(self, epoch):
+
+        # 计算所需的批次数量 ceil用于向上取整
+        steps = int(np.ceil(args.dataset_size / args.batch))
+
+        for i in tqdm(range(steps)):
+            # 1. 该 batch 的起始下标
+            st = i * args.batch
+            ed = min((i + 1) * args.batch, args.dataset_size)
+            bt = ed - st
+
+            # 2. 构造待训练的 batch 数据: batch, box_num, 3
+            batch_trn = self.handler.datalist[i] 
+            box_num = batch_trn.shape[1]
+            batch_trn = torch.Tensor(batch_trn).to(args.device)
+            state = StatePack3D(batch_size=bt, box_num=box_num)
+            state.update_env(batch_trn, bt)
+            
+            # 3. 训练该 batch
+            state, values, returns, losses, entropy, grad_norms = self.train_instance(state)  
+            epoch_logger(epoch, state, values, returns, losses, entropy, grad_norms)
+
+    def train_instance(self, state):
+        total_losses = torch.tensor([0,0,0,0], dtype=torch.float, device=args.device)
+        total_entropy = torch.zeros(1, dtype=torch.float, device=args.device)
+        update_mb_number=int(state.blocks_num//args.nsteps)
+        for mb_id in tqdm(range(update_mb_number), disable=True):
+            state, entropy, values, returns, losses, grad_norms = self.train_minibatch(state)
+
+            total_entropy += entropy
+            total_losses += losses
+        
+        average_entropy = total_entropy / update_mb_number
+        average_losses = total_losses / update_mb_number
+
+        return state, values, returns, average_losses, average_entropy, grad_norms
+    
+    def train_minibatch(self, state):
+        modules = self.model
+        returns, advs, values, log_likelihoods, entropys = self.get_mb_data(state)
+
+        alpha_loss = -1 * torch.mv((-1*entropys + args.target_entropy).detach(), modules['critic'].module.log_alpha).mean()
+
+        value_loss = F.mse_loss(values, returns.float().detach())
+
+        advs = returns - values
+        # Normalize the advantages
+        advs = (advs - advs.mean()) / (advs.std() + 1e-8)
+        # do not backward critic in actor
+        advantages = advs.detach()
+        # Calculate loss (gradient ascent)  loss=A*log
+        actor_loss = -1 * (advantages * log_likelihoods).mean()
+        
+        loss = actor_loss + value_loss + alpha_loss
+
+        # TODO: Clip gradient norms
+        if args.full_eval_mode == 0:
+            # Perform backward pass and optimization step
+            self.optimizer.zero_grad()
+            loss.backward()
+            # Clip gradient norms and get (clipped) gradient norms for logging
+            grad_norms = clip_grad_norms(self.optimizer.param_groups, args.grad_clip)
+            self.optimizer.step()
+
+            if self.scheduler is not None:
+                self.scheduler.step()
+        else:
+            grad_norms = None
+
+        losses = torch.tensor([actor_loss, value_loss, alpha_loss, loss], device=args.device)
+
+        return state, entropys.mean(), values, returns, losses, grad_norms
+    
+    def get_mb_data(self, state):
+    
+        def sf01(arr):
+            """
+            swap and then flatten axes 0 and 1
+            """
+            s = arr.size()
+            return arr.transpose(0, 1).reshape(s[0] * s[1], *s[2:]) 
+
+        mb_rewards, mb_values, mb_log_likelihoods, mb_entropy = [],[],[],[]
+        modules = self.model
+
+        for _ in range(args.nsteps):
+            
+            ll, entropy, value, reward = self._run_batch(state)
+            mb_values.append(value)
+            mb_log_likelihoods.append(ll)
+            mb_rewards.append(reward)
+            mb_entropy.append(entropy)
+
+        # batch of steps to batch of roll-outs (nstep, batch)
+        mb_rewards = torch.stack(mb_rewards)
+        mb_values = torch.stack(mb_values)
+        mb_log_likelihoods = torch.stack(mb_log_likelihoods)
+        # (nstep, batch, 3)
+        mb_entropys = torch.stack(mb_entropy)
+        if (state.packed_state[:,:,0]==1).all():
+            last_values=torch.zeros(state.batch_size,dtype=torch.float)
+            last_values=move_to(last_values,state.device)
+        else:
+            hm = np.zeros((state.batch_size, 2, args.bin_x, args.bin_y)).astype(int)
+            for i in range(state.batch_size):
+                hm_diff_x = np.insert(state.heightmap[i], 0, state.heightmap[i][0, :], axis=0)
+                hm_diff_x = np.delete(hm_diff_x, len(hm_diff_x) - 1, axis=0)
+                hm_diff_x = state.heightmap[i] - hm_diff_x
+
+                hm_diff_y = np.insert(state.heightmap[i], 0, state.heightmap[i][:, 0], axis=1)
+                hm_diff_y = np.delete(hm_diff_y, len(hm_diff_y.T) - 1, axis=1)
+                hm_diff_y = state.heightmap[i] - hm_diff_y
+                # combine 
+                hm[i][0] = hm_diff_x
+                hm[i][1] = hm_diff_y
+
+            hm = torch.tensor(hm).float()
+            hm = move_to(hm, state.device)
+            actor_modules = modules['actor']
+
+            actor_encoder_out = actor_modules['encoder'](state.packed_state)
+            actor_encoderheightmap_out = actor_modules["encoderheightmap"](hm)
+            last_values = modules['critic'](actor_encoderheightmap_out, actor_encoder_out)
+            last_values = last_values.squeeze(-1).squeeze(-1)
+
+        mb_returns = torch.zeros_like(mb_rewards)
+        mb_advs = torch.zeros_like(mb_rewards)
+
+        lastgaelam = 0
+
+        for t in reversed(range(args.nsteps)):
+            if t == args.nsteps - 1:
+                nextvalues = last_values
+            else:
+                nextvalues = mb_values[t+1]
+
+            delta = mb_rewards[t] + args.gamma * nextvalues - mb_values[t]
+            mb_advs[t] = lastgaelam = delta + args.gamma * args.lam * lastgaelam
+
+        # use return to supervise critic
+        mb_returns = mb_advs + mb_values
+
+        # (batch * nstep, )
+        returns, advs, values, log_likelihoods, entropys = map(sf01, (mb_returns, mb_advs, mb_values, mb_log_likelihoods, mb_entropys))
+
+        return  returns, advs, values, log_likelihoods, entropys
+    
+
+    def _run_batch(self, state):
+        modules = self.model
+        # update pack candidates for next packing step
+        last_gap = state.get_gap_size()
+
+        s_log_p, r_log_p, value= pack_step(self.model, state)
+
+        actions = state.action()
+        
+        ll = self.loglikelihood(actions, s_log_p, r_log_p)
+        entropys = self.entropy(s_log_p, r_log_p)
+
+        # (batch)
+        new_gap = state.get_gap_size()
+        reward = (last_gap - new_gap)
+
+        alpha = torch.exp(modules['critic'].log_alpha)
+
+        reward +=  torch.mv(entropys, alpha).detach()
+
+        state.put_reward(reward)
+
+        return ll, entropys, value, reward
+    
+def epoch_logger(epoch, state, values, returns, losses, entropy, grad_norms):
+    gap_ratio = state.get_gap_ratio()
+    rewards = state.get_rewards()
+    
+    avg_gap_ratio = gap_ratio.mean().item()
+    var_gap_ratio = gap_ratio.var().item()
+    avg_rewards = rewards.mean().item()
+    min_gap = torch.min(gap_ratio)
+    max_gap = torch.max(gap_ratio)
+
+    grad_norms, grad_norms_clipped = grad_norms
+
+    ev = explained_variance(values.detach().cpu().numpy(), returns.detach().cpu().numpy())
+
+    # Log values to screen
+    if epoch % args.log_interval == 0:
+        print("state.packed_state:{}".format(state.packed_state))
+        print('\nepoch: {}, avg_rewards: {}, gap_ratio: {}, var_gap_ratio: {}, ev: {}, loss: {}'.\
+              format(epoch, avg_rewards, avg_gap_ratio, var_gap_ratio, ev, losses[3]))
+        print('min gap ratio: {}, max gap ratio: {}'.format(min_gap, max_gap))
+        print('grad_norm: {}, clipped: {}'.format(grad_norms[0], grad_norms_clipped[0]))
+        print('grad_norm_c: {}, clipped_c: {}'.format(grad_norms[1], grad_norms_clipped[1]))
+        
+        print("entropy:{}".format(entropy))
+    logging.info("epoch", epoch)
+    logging.info("explained_variance", float(ev))
+    logging.info('entropy', entropy.item())
+        
+    logging.info('actor_loss', losses[0].item())
+    logging.info('value_loss', losses[1].item())
+    logging.info('alpha_loss', losses[2].item())
+    logging.info('avg_rewards', avg_rewards)
+    logging.info('gap_ratio', avg_gap_ratio)
+    logging.info('var_gap_ratio', var_gap_ratio)
+
+
